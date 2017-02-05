d2515b2164d4647c2e0660435f379ad3b9d8b032
Sttephy Blz <sttephyblz@gmail.com>
2017-02-05 16:24:04 -0700

    Variables y tipos

--- prueba.js
+++ prueba.js
@@ -5,6 +5,12 @@ var nombre = "Stephy";
 var resultado = holaMundo(nombre);
 var etiqueta = document.getElementById("container");
 etiqueta.innerHTML = resultado;
+//Variables y tipos
+var nombre2 = "John"; //Cualquier tipo de datos.
+var edad = 21;
+var programador = true;
+var langs = ["PHP", "JavaScript", "CSS"];
+etiqueta.innerHTML = nombre2 + " - " + edad;
 alert("Hola mundo alert");
 //Se compila en consola el TS:
 //tsc <nombre del ficher>.ts
--- prueba.ts
+++ prueba.ts
@@ -8,7 +8,17 @@ var resultado = holaMundo(nombre);
 var etiqueta = <HTMLElement>document.getElementById("container");
 etiqueta.innerHTML = resultado;
 
+
+//Variables y tipos
+var nombre2:any = "John"; //Cualquier tipo de datos.
+var edad:number = 21;
+var programador:boolean = true;
+var langs: Array<string> = ["PHP", "JavaScript", "CSS"];
+
+etiqueta.innerHTML =nombre2+" - "+edad;
+
 alert("Hola mundo alert");
+
 //Se compila en consola el TS:
 //tsc <nombre del ficher>.ts
 //tsc -w *.ts -> Checa continuamente las modificaciones
