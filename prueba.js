e7c525dd097150dd39e5f6b126ffe155793d9281
Sttephy Blz <sttephyblz@gmail.com>
2017-02-05 16:13:34 -0700

    Hola mundo

---
+++ prueba.js
@@ -0,0 +1,11 @@
+function holaMundo(nombre) {
+    return "Hola mundo. Soy " + nombre;
+}
+var nombre = "Stephy";
+var resultado = holaMundo(nombre);
+var etiqueta = document.getElementById("container");
+etiqueta.innerHTML = resultado;
+alert("Hola mundo alert");
+//Se compila en consola el TS:
+//tsc <nombre del ficher>.ts
+//tsc -w *.ts -> Checa continuamente las modificaciones
