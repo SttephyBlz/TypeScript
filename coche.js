ac133634d8ade98930d6b0a5f50a4fb2f9249251
Sttephy Blz <sttephyblz@gmail.com>
2017-02-05 17:12:44 -0700

    Clases y métodos 2

--- coche.js
+++ coche.js
@@ -1,11 +1,21 @@
 var Coche = (function () {
     function Coche() {
+        this.velocidad = 0;
     }
     Coche.prototype.getColor = function () {
         return this.color;
     };
     Coche.prototype.setColor = function (color) {
         this.color = color;
+    };
+    Coche.prototype.getVelocidad = function () {
+        return this.velocidad;
+    };
+    Coche.prototype.acelerar = function () {
+        this.velocidad++;
+    };
+    Coche.prototype.frenar = function () {
+        this.velocidad--;
     };
     return Coche;
 }());
@@ -19,3 +29,9 @@ coche3.setColor("Verde");
 console.log("El color del coche 1 es: " + coche.getColor());
 console.log("El color del coche 2 es: " + coche2.getColor());
 console.log("El color del coche 3 es: " + coche3.getColor());
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
+console.log("La velocidad del coche 1 es: " + coche.getVelocidad());
+coche.frenar();
+console.log("La velocidad después de frenar el coche 1 es: " + coche.getVelocidad());
--- coche.ts
+++ coche.ts
@@ -1,7 +1,7 @@
 class Coche{
   public color: string;
   public modelo: string;
-  public velocidad: number;
+  public velocidad: number =0;
 
   public getColor(){
     return this.color;
@@ -9,6 +9,18 @@ class Coche{
 
   public setColor(color: string){
     this.color = color;
+  }
+
+  public getVelocidad():number{
+    return this.velocidad;
+  }
+
+  public acelerar(){
+    this.velocidad++;
+  }
+
+  public frenar(){
+    this.velocidad--;
   }
 }
 
@@ -24,3 +36,12 @@ coche3.setColor("Verde");
 console.log("El color del coche 1 es: "+coche.getColor());
 console.log("El color del coche 2 es: "+coche2.getColor());
 console.log("El color del coche 3 es: "+coche3.getColor());
+
+coche.acelerar();
+coche.acelerar();
+coche.acelerar();
+
+console.log("La velocidad del coche 1 es: "+coche.getVelocidad());
+
+coche.frenar();
+console.log("La velocidad después de frenar el coche 1 es: "+coche.getVelocidad());
